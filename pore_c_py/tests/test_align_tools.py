a23425e1cfb818157803f1bed0d4918f6417351d
Chris Wright <chris.wright@nanoporetech.com>
2023-03-06 09:53:17 +0000

    Mass extinction

--- pore_c_py/tests/test_align_tools.py
+++ pore_c_py/tests/test_align_tools.py
@@ -69,3 +69,94 @@ def test_group_colinear(aligns, expected):
         exp.append([alns[x] for x in grp])
     res = align_tools.group_colinear(alns)
     assert res == exp
+
+
+# @pytest.mark.parametrize(
+#     "left,right,expected",
+#     [
+#         (("+", 0, 5), ("+", 10, 15), 5),  # span 5-10
+#         (("-", 10, 15), ("-", 0, 5), -5),  # span 5-10
+#         (("-", 0, 5), ("+", 10, 15), 10),  # span 0-10
+#         (("-", 10, 15), ("+", 0, 5), -10),  # span 0-10
+#         (("+", 0, 5), ("-", 10, 15), 10),  # span 5-15
+#         (("+", 10, 15), ("-", 0, 5), -10),  # span 5-15
+#         (("-", 0, 5), ("-", 10, 15), 15),  # span 0-15
+#         (("+", 10, 15), ("+", 0, 5), -15),  # span 0-15
+#     ],
+# )
+# def test_genomic_distance(left, right, expected):
+#     """Test genomic distance."""
+#     assert calculate_genomic_distance(left, right) == expected
+#
+#
+# def test_short_walk(m1, caplog):
+#     """Test short walk."""
+#     m1[2].read_seq.flags.secondary = True
+#     with caplog.at_level(logging.WARNING):
+#         m = list(
+#             annotate_monomer_alignments(m1[:-1], remove_qcfail=False))[0][1]
+#         qcfail = [_.read_seq.flags.qcfail for _ in m]
+#         assert qcfail == [False, False, True]
+#         assert (
+#             caplog.record_tuples[-1][2]
+#             == "Expected to see 3 alignments for concatemer C, found 2"
+#         )
+#
+#
+# @pytest.fixture
+# def m2():
+#     """Make mock monomer aligns m2."""
+#     return (
+#         _make_mock_monomer_aligns(
+#             [
+#                 (0, 5, "chr1", 0, None),
+#                 (5, 10, "chr2", 100, None),
+#                 (20, 25, "chr1", 50, None),
+#                 (25, 30, "chr1", 500, None),
+#             ]
+#         ),
+#         [
+#             PairData(*_)
+#             for _ in [
+#                 ("C", 0, True, 0, PairAlignState.both, False, None, None),
+#                 ("C", 1, True, 10, PairAlignState.both, False, None, None),
+#                 ("C", 2, True, 0, PairAlignState.both, True, 445, True),
+#             ]
+#         ],
+#     )
+#
+#
+# def test_pairs_direct(m2):
+#     """Test pairs direct."""
+#     aligns, expected = m2
+#     _, _, pair_data = zip(*get_pairs(aligns, direct_only=True))
+#     assert list(pair_data) == expected
+#
+#
+# @pytest.fixture
+# def m3():
+#     """Make mock monomer aligns m3."""
+#     return (
+#         _make_mock_monomer_aligns(
+#             [
+#                 (0, 5, "chr1", 0, None),
+#                 (5, 10, "chr2", 100, None),
+#                 (20, 25, "chr1", 50, None),
+#             ]
+#         ),
+#         [
+#             PairData(*_)
+#             for _ in [
+#                 ("C", 0, True, 0, PairAlignState.both, False, None, None),
+#                 ("C", 1, False, 15, PairAlignState.both, True, 45, True),
+#                 ("C", 2, True, 10, PairAlignState.both, False, None, None),
+#             ]
+#         ],
+#     )
+#
+#
+# def test_pairs_indirect(m3):
+#     """Test pairs indirect."""
+#     aligns, expected = m3
+#     _, _, pair_data = zip(*get_pairs(aligns, direct_only=False))
+#     assert list(pair_data) == expected
