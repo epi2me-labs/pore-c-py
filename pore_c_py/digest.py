a70ce85f03d3976f42a05754273e80cbf88e3094
Sarah Griffiths <sarah.griffiths@nanoporetech.com>
2023-05-02 15:39:16 +0000

    digest tests

--- pore_c_py/digest.py
+++ pore_c_py/digest.py
@@ -6,61 +6,60 @@ from Bio.Seq import Seq
 
 from pore_c_py import utils
 
+
 logger = utils.get_named_logger("Digest")
 
 
-def get_subread(align, start, end):
-    """Get sub read."""
+def get_subread_modified_bases(align, start, end):
+    """Get modified bases subread.
+
+    :param align: pysam.AlignedSegment.
+    :param start: start coordinate to trim to.
+    :param end: exclusive end co-ordinate.
+    """
+    mm_str, ml_str = "", ""
+    base_indices = {}
     seq = align.query_sequence[start:end]
-    if align.query_qualities:
-        qual = align.query_qualities[start:end]
-    else:
-        qual = None
-    if align.modified_bases:
-        mm_str, ml_str = "", ""
-        base_indices = {}
-        for mod_key, mod_data in align.modified_bases.items():
-            # find the modifications that overlap the subread
-            idx = [
-                x for x in range(len(mod_data)) if
-                start <= mod_data[x][0] < end]
-            if not idx:  # no mods in this subread
-                continue
-            try:
-                canonical_base, strand, skip_scheme, mod_type = mod_key
-            except ValueError:
-                canonical_base, strand, mod_type = mod_key
-                skip_scheme = ""
-
-            canonical_base = mod_key[0]
-            # find the positions of that canonical base in the subread
-            if canonical_base not in base_indices:
-                base_indices[canonical_base] = [
-                    x for x, b in enumerate(seq) if b.upper() == canonical_base
-                ]
-
-            base_offsets, probs = zip(*[mod_data[_] for _ in idx])
-            deltas = []
-            counter = 0
-            for seq_idx in base_indices[canonical_base]:
-                orig_idx = seq_idx + start
-                if orig_idx in base_offsets:  # is modified
-                    deltas.append(str(counter))
-                    counter = 0
-                else:
-                    counter += 1
-            assert len(deltas) == len(probs)
+    for mod_key, mod_data in align.modified_bases.items():
+        # find the modifications that overlap the subread
+        idx = [
+            x for x in range(len(mod_data)) if
+            start <= mod_data[x][0] < end]
+        probs_dic = dict(mod_data)
+        if not idx:  # no mod bases (of this type) in the subread
+            continue
+        try:
+            canonical_base, strand, skip_scheme, mod_type = mod_key
+        except ValueError:
+            canonical_base, strand, mod_type = mod_key
+            skip_scheme = ""
+        if canonical_base == "N":
+            base_indices[canonical_base] = list(range(len(seq)))
+        elif canonical_base not in base_indices:
+            base_indices[canonical_base] = [
+                x for x, b in enumerate(seq) if b.upper() == canonical_base
+            ]
+        base_offsets, probs = zip(*[mod_data[i] for i in idx])
+        strand = "+" if strand == 0 else "-"
+        deltas = []
+        counter = 0
+        probs = []
+        for seq_idx in base_indices[canonical_base]:
+            orig_idx = seq_idx + start
+            if orig_idx in base_offsets:  # is modified
+                probs += [probs_dic[orig_idx]]
+                deltas.append(str(counter))
+                counter = 0
+            else:
+                counter += 1
+            deltas_formatted = ','.join(deltas)
             prob_str = ",".join(map(str, probs))
-            strand = "+" if strand == 0 else "-"
-            mm_str += (
+        mm_str += (
                 f"{canonical_base}{strand}{mod_type}{skip_scheme}"
-                f",{','.join(deltas)};"
+                f",{deltas_formatted};"
             )
-            ml_str += f"{canonical_base},{prob_str};"
-    else:
-        mm_str, ml_str = None, None
-
-    return seq, qual, mm_str, ml_str
+        ml_str += f"{prob_str},"
+    return mm_str, ml_str
 
 
 def splits_to_intervals(positions, length):
@@ -88,10 +87,57 @@ def get_enzyme(enzyme):
     return enz
 
 
-def get_concatemer_seqs(input_file, enzyme, remove_tags):
+def digest_sequence(align, enzyme, tags_remove=None):
+    """Digest sequence."""
+    # the move tag massively bloats files, and we don't care for
+    # it or handle it in trimming, so force its removal by default.
+    if tags_remove is None:
+        tags_remove = {'mv'}
+
+    concatemer_id = align.query_name
+    cut_points = [x - 1 for x in enzyme.search(Seq(align.query_sequence))]
+    read_length = len(align.query_sequence)
+    num_digits = len(str(read_length))
+    intervals = splits_to_intervals(cut_points, read_length)
+    num_intervals = len(intervals)
+
+    for idx, (start, end) in enumerate(intervals):
+        read = copy.copy(align)
+        # trim the sequence and quality
+        seq = align.query_sequence[start:end]
+        qual = None
+        if align.query_qualities:
+            qual = align.query_qualities[start:end]
+        read.query_sequence = seq
+        read.query_qualities = qual
+        # deal with mods, upgrading tag from interim to approved spec
+        if ('Mm' in tags_remove) or ('MM' in tags_remove):
+            # Setting to None effectively deletes,
+            # or does nothing if not present
+            for tag in ('Mm', 'Ml', 'MM', 'ML'):
+                read.set_tag(tag, None)
+        else:
+            mm, ml = get_subread_modified_bases(align, start, end)
+            for tag in ('Mm', 'Ml'):
+                read.set_tag(tag, None)
+            read.set_tag("MM", mm)
+            read.set_tag("ML", ml)
+        # lexograpically sortable monomer ID
+        read.query_name = \
+            f"{concatemer_id}:{start:0{num_digits}d}:{end:0{num_digits}d}"
+        read.set_tag(
+            utils.MONOMER_DATA_TAG,
+            [start, end, read_length, idx, num_intervals])
+        utils.MonomerData.set_monomer_data(
+                read, start, end, read_length, idx, num_intervals)
+        read.set_tag(utils.CONCATEMER_ID_TAG, concatemer_id, "Z")
+        yield read
+
+
+def get_concatemer_seqs(input_file, enzyme, remove_tags=None):
     """Digest concatemers in to unaligned monomers.
 
-    :param input_file: The input BAM or Fastq file.
+    :param input_file: pysam.AlignmentFile input
     :param enzyme: Name of the digestion enzyme used in sample preperation
     :param remove_tags: Comma seperated list of additional
                         tags to remove from file
@@ -103,34 +149,15 @@ def get_concatemer_seqs(input_file, enzyme, remove_tags):
     n_concatemers = 0
     n_monomers = 0
     enzyme = get_enzyme(enzyme)
-    tags_remove = {"Ml", "ML", "Mm", "MM", "mv"}
+    tags_remove = {"mv"}
     if remove_tags:
         tags_remove.update(set(remove_tags))
     for align in input_file.fetch(until_eof=True):
         n_concatemers += 1
-        concatemer_id = align.query_name
-        cut_points = [x - 1 for x in enzyme.search(Seq(align.query_sequence))]
-        read_length = len(align.query_sequence)
-        num_digits = len(str(read_length))
-        intervals = splits_to_intervals(cut_points, read_length)
-        num_intervals = len(intervals)
-        for idx, (start, end) in enumerate(intervals):
+        reads = digest_sequence(
+            align, enzyme, tags_remove)
+        for read in reads:
             n_monomers += 1
-            read = copy.copy(align)
-            seq, qual, mm_str, ml_str = get_subread(
-                align=align, start=start, end=end)
-            for item in tags_remove:
-                read.set_tag(item, None)
-            read.query_sequence = seq
-            read.query_qualities = qual
-            # lexograpically sortable monomer ID
-            read.query_name = \
-                f"{concatemer_id}:{start:0{num_digits}d}:{end:0{num_digits}d}"
-            utils.MonomerData.set_monomer_data(
-                read, start, end, read_length, idx, num_intervals)
-            read.set_tag(utils.CONCATEMER_ID_TAG, concatemer_id, "Z")
-            read.set_tag("MM", mm_str)
-            read.set_tag("ML", ml_str)
             yield read
-
-    logger.info(f"Found {n_monomers} monomers in {n_concatemers} concatemers.")
+    logger.info(
+        f"Found {n_monomers} monomers in {n_concatemers} concatemers.")
--- tests/test_digest.py
+++ tests/test_digest.py
@@ -1,8 +1,22 @@
 """Test functions from digest."""
+import copy
+
+import pysam
 import pytest
 
 from pore_c_py import digest
+from array import array
+from tests.test_align_tools import align_from_tuple
 
+
+def align_with_sequence(t, query_sequence=None, name="test"):
+    """Align from tuple with specific sequence."""
+    rec = align_from_tuple(t)
+    if query_sequence is not None:
+        rec.query_sequence = query_sequence
+    rec.query_name = name
+    rec.qual = "&" * (t[2] - t[1])
+    return rec
 
 @pytest.mark.parametrize(
     "length,positions,expected",
@@ -19,3 +33,106 @@ def test_splits_to_intervals(length, positions, expected):
     """Test splits to intervals."""
     intervals = digest.splits_to_intervals(positions, length)
     assert intervals == expected
+
+
+# https://github.com/pysam-developers/pysam/blob/b0f1eb7f71b8f5305b112d26da5f72ce79dc33b3/tests/pysam_data/MM-chebi.sam
+example1 = (
+    ("*", 0, 36, "+"),
+    # . = m
+    # , = h (76792)
+    # ; = m and h
+    # | = N
+    #      .        | .|,.          .  ;
+    "AGCACTCCAGAGTCGNACGCCATYCGCGCGCCACCA",
+    "C+m,2,2,1,4,1;C+76792,6,7;N+n,15,2;",
+    # .   .   .   .   .   ,   ,   |   |
+    [102,128,153,179,204,161,187,212,169])
+
+@pytest.mark.parametrize(
+    "name, sam_data, start, end, mm_exp, ml_exp",
+    [
+        # the whole read
+        ("whole", example1, 0, 36, "C+m,2,2,1,4,1;C+76792,6,7;N+n,15,2;",
+         [102, 128, 153, 179, 204, 161, 187, 212, 169]),
+
+        # trim two non-C bases.
+        # Doesn't effect C mods, rejigs coords of N
+        ("trim-0C", example1, 2, 36, "C+m,2,2,1,4,1;C+76792,6,7;N+n,13,2;",
+         [102, 128, 153, 179, 204, 161, 187, 212, 169]),
+
+        # trim first C
+        # Need to skip one less C for C mod subtag and for N (since its any base)
+        ("trim-1C", example1, 3, 36, "C+m,1,2,1,4,1;C+76792,5,7;N+n,12,2;",
+         [102, 128, 153, 179, 204, 161, 187, 212, 169]),
+
+        # trim first two C
+        # We now skip 0 Cs before first mod.
+        ("trim-2C", example1, 5, 36, "C+m,0,2,1,4,1;C+76792,4,7;N+n,10,2;",
+         [102, 128, 153, 179, 204, 161, 187, 212, 169]),
+
+        # trim first three C
+        # We've trimmed a mod base so drop the first entry from ML. Also
+        ("trim-3C", example1, 7, 36, "C+m,2,1,4,1;C+76792,3,7;N+n,8,2;",
+         [128, 153, 179, 204, 161, 187, 212, 169]),
+
+        # trim last C
+        # Affects both m and h codes
+        ("trim+1C", example1, 0, 34, "C+m,2,2,1,4;C+76792,6;N+n,15,2;",
+         [102, 128, 153, 179, 161, 212, 169]),
+
+        # trim past first N
+        # Affects both m and h codes also
+        ("trim-5C", example1, 17, 36, "C+m,0,1,4,1;C+76792,1,7;N+n,1;",
+         [128,153,179,204,161,187,169]),
+
+        # trim past first N and last C
+        # Affects both m and h codes also
+        ("trim-5C+1C", example1, 17, 34, "C+m,0,1,4;C+76792,1;N+n,1;",
+         [128,153,179,161,169]),
+
+    ],
+)
+def test_get_subread_modified_bases(
+        name, sam_data, start, end, mm_exp, ml_exp):
+    """Test get subread."""
+    # note: the representation isn't unique and the code doesn't produce the same
+    #       representation as the test data. So we parse both using pysam and compare
+
+    # create original
+    aln_tuple, qseq, mm, ml = sam_data
+    aln = align_with_sequence(aln_tuple, query_sequence=qseq, name="*")
+    aln.set_tag("Mm", mm)
+    aln.set_tag("Ml", ml)
+
+    # create trimmed
+    read = copy.copy(aln)
+    mm, ml = digest.get_subread_modified_bases(aln, start, end)
+    read.set_tag("Mm", mm)
+    parse_ml = list(map(int, ml[:-1].split(',')))
+    read.set_tag("Ml", parse_ml)
+
+    # original with expected tags
+    expected_aln = align_with_sequence(aln_tuple, query_sequence=qseq, name="*")
+    expected_aln.set_tag("Mm", mm_exp)
+    expected_aln.set_tag("Ml", ml_exp)
+
+    # compare
+    for k, v in read.modified_bases.items():
+        assert expected_aln.modified_bases[k] == v
+
+
+@pytest.mark.parametrize(
+    "align, sequence, enzyme, expected",
+    [
+        (("chr1", 0, 19, "+"), 'AAACATGTTTGGCATGAAA', "NlaIII",
+         ['AAACATG', 'TTTGGCATG', 'AAA']),
+        (("chr1", 0, 10, "+"), 'GGATCTGATC', "DpnII",
+         ['G', 'GATCT', 'GATC'])
+    ],
+)
+def test_digest_sequence(align, sequence, enzyme, expected):
+    aln = align_with_sequence(align, query_sequence=sequence)
+    enz = digest.get_enzyme(enzyme)
+    seqs = digest.digest_sequence(aln, enz)
+    for i, gen in enumerate(seqs):
+        assert gen.query_sequence == expected[i]
